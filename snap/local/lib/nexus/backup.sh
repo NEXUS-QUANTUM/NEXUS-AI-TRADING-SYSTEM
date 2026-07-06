09a2a958a41c0de912af06901f9d1f26667300e4
Dr Xenon <drxenon487@gmail.com>
2026-07-06 02:02:46 +0000

    Delete  directory

--- snap/local/lib/nexus/backup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# NEXUS AI TRADING SYSTEM
-# Copyright © 2026 NEXUS QUANTUM LTD
-# CEO: Dr X... - Majority Shareholder
-
--- snap/local/share/nexus/examples/alert_handler.py
+++
@@ -1,6 +0,0 @@
-"""
-NEXUS AI TRADING SYSTEM
-Copyright © 2026 NEXUS QUANTUM LTD
-CEO: Dr X... - Majority Shareholder
-"""
-
