c00bd8eda41b420c9cffbf7a8c614325f4230da5
NEXUS QUANTUM <drxenon487@gmail.com>
2026-07-10 00:21:57 +0000

    NEXUS

---
+++ apps/web/src/components/common/index.ts
@@ -0,0 +1,175 @@
+/**
+ * NEXUS AI TRADING SYSTEM
+ * Copyright © 2026 NEXUS QUANTUM LTD
+ * CEO: Dr X... - Majority Shareholder
+ */
+
+// ============================================
+// EXPORTATIONS DE TOUS LES COMPOSANTS COMMON
+// ============================================
+
+// Layout & Structure
+export { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from './Accordion';
+export { Alert, AlertTitle, AlertDescription } from './Alert';
+export { AlertDialog, AlertDialogPortal, AlertDialogOverlay, AlertDialogTrigger, AlertDialogContent, AlertDialogHeader, AlertDialogFooter, AlertDialogTitle, AlertDialogDescription, AlertDialogAction, AlertDialogCancel } from './AlertDialog';
+export { AspectRatio } from './AspectRatio';
+export { Avatar, AvatarImage, AvatarFallback } from './Avatar';
+export { AvatarGroup } from './AvatarGroup';
+export { AvatarUpload } from './AvatarUpload';
+export { Badge } from './Badge';
+export { Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbLink, BreadcrumbPage, BreadcrumbSeparator } from './Breadcrumb';
+export { Button } from './Button';
+export { Calendar } from './Calendar';
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from './Card';
+export { Carousel, CarouselContent, CarouselItem, CarouselPrevious, CarouselNext } from './Carousel';
+export { Checkbox } from './Checkbox';
+export { Collapsible, CollapsibleTrigger, CollapsibleContent } from './Collapsible';
+export { Command, CommandDialog, CommandInput, CommandList, CommandEmpty, CommandGroup, CommandItem, CommandShortcut, CommandSeparator } from './Command';
+export { ContextMenu, ContextMenuTrigger, ContextMenuContent, ContextMenuItem, ContextMenuCheckboxItem, ContextMenuRadioItem, ContextMenuLabel, ContextMenuSeparator, ContextMenuShortcut, ContextMenuGroup, ContextMenuPortal, ContextMenuSub, ContextMenuSubContent, ContextMenuSubTrigger, ContextMenuRadioGroup } from './ContextMenu';
+export { DatePicker } from './DatePicker';
+export { Dialog, DialogPortal, DialogOverlay, DialogTrigger, DialogClose, DialogContent, DialogHeader, DialogFooter, DialogTitle, DialogDescription } from './Dialog';
+export { Drawer, DrawerPortal, DrawerOverlay, DrawerTrigger, DrawerClose, DrawerContent, DrawerHeader, DrawerFooter, DrawerTitle, DrawerDescription } from './Drawer';
+export { Dropdown } from './Dropdown';
+export { DropdownMenu, DropdownMenuTrigger, DropdownMenuGroup, DropdownMenuPortal, DropdownMenuSub, DropdownMenuRadioGroup, DropdownMenuSubTrigger, DropdownMenuSubContent, DropdownMenuContent, DropdownMenuItem, DropdownMenuCheckboxItem, DropdownMenuRadioItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuShortcut } from './DropdownMenu';
+export { Form, FormItem, FormLabel, FormControl, FormDescription, FormMessage, FormField } from './Form';
+export { HoverCard, HoverCardTrigger, HoverCardContent } from './HoverCard';
+export { Image } from './Image';
+export { Input } from './Input';
+export { Label } from './Label';
+export { Link } from './Link';
+export { Menubar, MenubarMenu, MenubarTrigger, MenubarContent, MenubarItem, MenubarSeparator, MenubarLabel, MenubarCheckboxItem, MenubarRadioGroup, MenubarRadioItem, MenubarPortal, MenubarSubContent, MenubarSubTrigger, MenubarGroup } from './Menubar';
+export { Modal } from './Modal';
+export { NavigationMenu, NavigationMenuList, NavigationMenuItem, NavigationMenuContent, NavigationMenuTrigger, NavigationMenuLink, NavigationMenuIndicator, NavigationMenuViewport } from './NavigationMenu';
+export { Pagination, PaginationContent, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious, PaginationEllipsis } from './Pagination';
+export { Popover, PopoverTrigger, PopoverContent } from './Popover';
+export { Portal } from './Portal';
+export { Progress } from './Progress';
+export { RadioGroup, RadioGroupItem } from './RadioGroup';
+export { Resizable, ResizablePanelGroup, ResizablePanel, ResizableHandle } from './Resizable';
+export { ScrollArea, ScrollBar } from './ScrollArea';
+export { Select, SelectGroup, SelectValue, SelectTrigger, SelectContent, SelectItem, SelectLabel, SelectSeparator } from './Select';
+export { Separator } from './Separator';
+export { Sheet, SheetPortal, SheetOverlay, SheetTrigger, SheetClose, SheetContent, SheetHeader, SheetFooter, SheetTitle, SheetDescription } from './Sheet';
+export { Skeleton } from './Skeleton';
+export { Slider } from './Slider';
+export { Spinner } from './Spinner';
+export { Switch } from './Switch';
+export { Table, TableHeader, TableBody, TableFooter, TableHead, TableRow, TableCell, TableCaption } from './Table';
+export { Tabs, TabsList, TabsTrigger, TabsContent } from './Tabs';
+export { Textarea } from './Textarea';
+export { Toast, ToastProvider, ToastViewport, ToastAction, ToastClose, ToastTitle, ToastDescription } from './Toast';
+export { Toggle } from './Toggle';
+export { Tooltip, TooltipProvider, TooltipTrigger, TooltipContent } from './Tooltip';
+export { VisuallyHidden } from './VisuallyHidden';
+
+// ============================================
+// EXPORTATIONS PAR DÉFAUT
+// ============================================
+export { default as AccordionDefault } from './Accordion';
+export { default as AlertDefault } from './Alert';
+export { default as AlertDialogDefault } from './AlertDialog';
+export { default as AspectRatioDefault } from './AspectRatio';
+export { default as AvatarDefault } from './Avatar';
+export { default as AvatarGroupDefault } from './AvatarGroup';
+export { default as AvatarUploadDefault } from './AvatarUpload';
+export { default as BadgeDefault } from './Badge';
+export { default as BreadcrumbDefault } from './Breadcrumb';
+export { default as ButtonDefault } from './Button';
+export { default as CalendarDefault } from './Calendar';
+export { default as CardDefault } from './Card';
+export { default as CarouselDefault } from './Carousel';
+export { default as CheckboxDefault } from './Checkbox';
+export { default as CollapsibleDefault } from './Collapsible';
+export { default as CommandDefault } from './Command';
+export { default as ContextMenuDefault } from './ContextMenu';
+export { default as DatePickerDefault } from './DatePicker';
+export { default as DialogDefault } from './Dialog';
+export { default as DrawerDefault } from './Drawer';
+export { default as DropdownDefault } from './Dropdown';
+export { default as DropdownMenuDefault } from './DropdownMenu';
+export { default as FormDefault } from './Form';
+export { default as HoverCardDefault } from './HoverCard';
+export { default as ImageDefault } from './Image';
+export { default as InputDefault } from './Input';
+export { default as LabelDefault } from './Label';
+export { default as LinkDefault } from './Link';
+export { default as MenubarDefault } from './Menubar';
+export { default as ModalDefault } from './Modal';
+export { default as NavigationMenuDefault } from './NavigationMenu';
+export { default as PaginationDefault } from './Pagination';
+export { default as PopoverDefault } from './Popover';
+export { default as PortalDefault } from './Portal';
+export { default as ProgressDefault } from './Progress';
+export { default as RadioGroupDefault } from './RadioGroup';
+export { default as ResizableDefault } from './Resizable';
+export { default as ScrollAreaDefault } from './ScrollArea';
+export { default as SelectDefault } from './Select';
+export { default as SeparatorDefault } from './Separator';
+export { default as SheetDefault } from './Sheet';
+export { default as SkeletonDefault } from './Skeleton';
+export { default as SliderDefault } from './Slider';
+export { default as SpinnerDefault } from './Spinner';
+export { default as SwitchDefault } from './Switch';
+export { default as TableDefault } from './Table';
+export { default as TabsDefault } from './Tabs';
+export { default as TextareaDefault } from './Textarea';
+export { default as ToastDefault } from './Toast';
+export { default as ToggleDefault } from './Toggle';
+export { default as TooltipDefault } from './Tooltip';
+export { default as VisuallyHiddenDefault } from './VisuallyHidden';
+
+// ============================================
+// TYPES
+// ============================================
+export type { AccordionProps } from './Accordion';
+export type { AlertProps, AlertTitleProps, AlertDescriptionProps } from './Alert';
+export type { AlertDialogProps } from './AlertDialog';
+export type { AspectRatioProps } from './AspectRatio';
+export type { AvatarProps, AvatarImageProps, AvatarFallbackProps } from './Avatar';
+export type { AvatarGroupProps } from './AvatarGroup';
+export type { AvatarUploadProps } from './AvatarUpload';
+export type { BadgeProps } from './Badge';
+export type { BreadcrumbProps } from './Breadcrumb';
+export type { ButtonProps } from './Button';
+export type { CalendarProps } from './Calendar';
+export type { CardProps } from './Card';
+export type { CarouselProps } from './Carousel';
+export type { CheckboxProps } from './Checkbox';
+export type { CollapsibleProps } from './Collapsible';
+export type { CommandProps } from './Command';
+export type { ContextMenuProps } from './ContextMenu';
+export type { DatePickerProps } from './DatePicker';
+export type { DialogProps } from './Dialog';
+export type { DrawerProps } from './Drawer';
+export type { DropdownProps } from './Dropdown';
+export type { DropdownMenuProps } from './DropdownMenu';
+export type { FormProps, FormItemProps, FormLabelProps, FormControlProps, FormDescriptionProps, FormMessageProps, FormFieldProps } from './Form';
+export type { HoverCardProps } from './HoverCard';
+export type { ImageProps } from './Image';
+export type { InputProps } from './Input';
+export type { LabelProps } from './Label';
+export type { LinkProps } from './Link';
+export type { MenubarProps } from './Menubar';
+export type { ModalProps } from './Modal';
+export type { NavigationMenuProps } from './NavigationMenu';
+export type { PaginationProps } from './Pagination';
+export type { PopoverProps } from './Popover';
+export type { PortalProps } from './Portal';
+export type { ProgressProps } from './Progress';
+export type { RadioGroupProps, RadioGroupItemProps } from './RadioGroup';
+export type { ResizableProps } from './Resizable';
+export type { ScrollAreaProps, ScrollBarProps } from './ScrollArea';
+export type { SelectProps, SelectGroupProps, SelectValueProps, SelectTriggerProps, SelectContentProps, SelectItemProps, SelectLabelProps, SelectSeparatorProps } from './Select';
+export type { SeparatorProps } from './Separator';
+export type { SheetProps } from './Sheet';
+export type { SkeletonProps } from './Skeleton';
+export type { SliderProps } from './Slider';
+export type { SpinnerProps } from './Spinner';
+export type { SwitchProps } from './Switch';
+export type { TableProps, TableHeaderProps, TableBodyProps, TableFooterProps, TableHeadProps, TableRowProps, TableCellProps, TableCaptionProps } from './Table';
+export type { TabsProps, TabsListProps, TabsTriggerProps, TabsContentProps } from './Tabs';
+export type { TextareaProps } from './Textarea';
+export type { ToastProps, ToastProviderProps, ToastViewportProps, ToastActionProps, ToastCloseProps, ToastTitleProps, ToastDescriptionProps } from './Toast';
+export type { ToggleProps } from './Toggle';
+export type { TooltipProps, TooltipProviderProps, TooltipTriggerProps, TooltipContentProps } from './Tooltip';
+export type { VisuallyHiddenProps } from './VisuallyHidden';
